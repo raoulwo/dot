5c03d0516f728818345d00decb9fb7daa2604fcc
Raoul Wograndl <wograndl.raoul@gmail.com>
2025-11-19 23:47:29 +0100

    config: set up vim tmux navigator

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -14,10 +14,10 @@ vim.keymap.set("n", "<up>", "<nop>")
 vim.keymap.set("n", "<right>", "<nop>")
 
 -- Easier window navigation
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+-- vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+-- vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+-- vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+-- vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 
 -- Maintain visual selection after indentation
 vim.keymap.set("v", "<", "<gv", { desc = "Unindent visual selection" })
