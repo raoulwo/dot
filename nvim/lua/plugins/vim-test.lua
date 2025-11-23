0f63c0988b436868011cb54f200b5fcd001e19f9
Raoul Wograndl <wograndl.raoul@gmail.com>
2025-11-23 23:22:09 +0100

    config: set up vim-test to use floaterm

--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -7,6 +7,17 @@ return {
 			vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>")
 			vim.keymap.set("n", "<leader>tl", ":TestLast<CR>")
 			vim.keymap.set("n", "<leader>tv", ":TestVisit<CR>")
+
+			vim.cmd([[
+                function! FloatermStrategy(cmd)
+                    execute 'silent FloatermKill'
+                    execute 'FloatermNew! '.a:cmd
+                    "" execute 'FloatermNew! '.a:cmd.' |less -X'
+                endfunction
+
+                let g:test#custom_strategies = {'floaterm': function('FloatermStrategy')}
+                let g:test#strategy = 'floaterm'
+            ]])
 		end,
 	},
 }
